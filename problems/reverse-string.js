c3c320429f7df4205e46b22d3a022fa2161c6aef
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-03-09 15:23:31 +0300

    finish phase 1

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,11 @@
 module.exports = function reverseString(string) {
   // Your code here
+  if (typeof string !== 'string') throw new TypeError('this input is not a string. please inter a string')
+
+  if (string.length === 1) return string
+
+  let restOfString = string.slice(0, string.length - 1) 
+  let lastCharecter = string[string.length - 1]
+
+  return lastCharecter + reverseString(restOfString)
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,14 @@
 // Your code here
+const {expect} = require('chai')
+const reverseString = require('../problems/reverse-string')
+
+
+describe('reverseString function', function() {
+    it('should return nuf from the string fun', function() {
+        expect(reverseString('fun')).to.equal('nuf')
+    })
+
+    it('should throw a TypeError if the given input is not a string', function() {
+        expect(() => reverseString(10).to.throw(TypeError))
+    })
+})
