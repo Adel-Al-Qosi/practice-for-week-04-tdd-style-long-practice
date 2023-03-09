e3ba2f4278e4e1501ded2523c2664628547c62e0
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-03-09 15:58:23 +0300

    finish phase 2

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,13 @@
 function returnsThree() {
   // Your code here
+  return 3
 }
 
 function reciprocal(n) {
   // Your code here
+  if (n < 1 || n > 1000000) throw RangeError('n should be between 1 and 1,000,000')
+
+  return 1/n
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,26 @@
 // Your code here
+const {expect} = require('chai')
+const functions = require('../problems/number-fun')
+
+const returnsThree = functions.returnsThree
+const reciprocal = functions.reciprocal
+
+describe('returnsThree function', function() {
+    it('should return the number 3', function() {
+        let three = returnsThree()
+        expect(three).to.equal(3)
+    })
+})
+
+describe('reciprocal function', function() {
+    it('should return the reciprocal of a number (1/number)', function() {
+        expect(reciprocal(4)).to.equal(1/4)
+        expect(reciprocal(50)).to.equal(1/50)
+        expect(reciprocal(10000)).to.equal(1/10000)
+    })
+
+    it('should only take numbers between 1 and 1,000,000', function() {
+        expect(reciprocal(10)).to.equal(1/10)
+        expect(() => reciprocal(0)).to.throw(RangeError)
+    })
+})
